fa153d3de80e7abb81ecfff0d1e7e96d3f28a642
Warren Kaye <wkaye@rds.com>
2024-12-13 11:13:27 -0800

    Ch3 midpoint.

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -13,9 +13,13 @@ namespace StarshipServer.Controllers
 
         private readonly ILogger<WeatherForecastController> _logger;
 
-        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        public WeatherForecastController(
+            ILogger<WeatherForecastController> logger,
+            IConfiguration configuration
+            )
         {
             _logger = logger;
+            var defaultLogLevel = configuration["Logging:LogLevel:Default"];
         }
 
         [HttpGet(Name = "GetWeatherForecast")]
